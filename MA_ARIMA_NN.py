dac585f9473695b6211a8e88b486fd3a9054e972
SooJungChae <naan_ace@naver.com>
2017-12-05 13:44:34 +0900

    Update MA_ARIMA_NN.py

--- MA_ARIMA_NN.py
+++ MA_ARIMA_NN.py
@@ -1,32 +1,66 @@
+''''
+Step1
+Make data stationary
+
+Step2
+ARIMA
+
+Step3
+ANN
+
+[Reference]
+[1] A comprehensive beginner’s guide to create a Time Series Forecast (with Codes in Python)
+    Link --> https://www.analyticsvidhya.com/blog/2016/02/time-series-forecasting-codes-python/
+''''
+
 from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score
+from datetime import datetime
+from matplotlib import pyplot as plt
+from statsmodels.sandbox.regression.predstd import wls_prediction_std
+
 import tensorflow as tf
 import scipy 
 import numpy as np
-import os
 import csv
 import math
 import pandas as pd
-from datetime import datetime
-from matplotlib import pyplot as plt
 import statsmodels.api as sm
-from sklearn.metrics import accuracy_score
-from statsmodels.sandbox.regression.predstd import wls_prediction_std
+
+fName = 'Sales_By_Weather_Time_Series_soo.csv'
+df = pd.read_csv(fName)
+
+df['SaleDate'] = pd.to_datetime(df['SaleDate'], format='%Y%m%d')
+new_df = df.set_index('SaleDate')
+
+ts = new_df['SaleQty']
+
+
+### Step1 - Make data stationary
+### I followed codes from reference [1]
+
+ts_log = np.log(ts)
+df_log = pd.DataFrame(ts_log)
+noinf_df_log = df_log.replace([np.inf, -np.inf], np.nan).dropna()
+ts_log = noinf_df_log['SaleQty']
+
+# Estimating & Eliminating trend. Moving Average
+moving_avg = ts_log.rolling(window=2,center=False).mean()
+
+
+
 
 Train_X = []; Train_Y = []
 Test_X = []; Test_Y = []
 
-fName = 'Sales_By_Weather_Time_Series_soo.csv'
-
-df = pd.read_csv(fName)
-
-h = 0
 row_count = 1133
 Train_cnt = round(row_count * 0.9)
 Test_cnt = round(row_count * 0.1)
 
 # Store Train and Test Data From CSV file
-for record in reader:
+h = 0
+for record in df:
   
   data = record[:-1]
   
@@ -35,7 +69,7 @@ for record in reader:
   if (h > Train_cnt):
     Test_X.append(list(map(float, data)))
     Test_Y.append(int(record[-1]))	# SaleQty
-    Date_Test.append(record[0])
+    Date_Test.append(record[0]) # SaleDate
   else:
     Train_X.append(list(map(float, data)))
     Train_Y.append(int(record[-1]))
